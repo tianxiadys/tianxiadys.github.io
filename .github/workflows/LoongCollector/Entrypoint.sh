9593e3da7d89cef25bc7a8c8668ff52b429d9e23
gaomingyu <874774259@qq.com>
2026-01-05 09:42:09 +0800

    fixed

--- .github/workflows/LoongCollector/Entrypoint.sh
+++ .github/workflows/LoongCollector/Entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ACCOUNT_ID=$(curl -s http://100.100.100.200/latest/meta-data/owner-account-id)
 REGION_ID=$(curl -s http://100.100.100.200/latest/meta-data/region-id)
-export ALIYUN_LOGTAIL_CONFIG="/app/conf/${REGION_ID}-inner/ilogtail_config.json"
+export ALIYUN_LOGTAIL_CONFIG="/app/${REGION_ID}-inner/ilogtail_config.json"
 export ALIYUN_LOGTAIL_USER_ID="${ACCOUNT_ID}"
 exec /app/loongcollector -enable_host_id=false -ilogtail_daemon_flag=false
