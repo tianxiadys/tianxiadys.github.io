9ac6c2a40b9d52f89a81941d52139345e3b48408
gaomingyu <874774259@qq.com>
2026-05-10 22:13:38 +0800

    fixed

--- crypto/crypto1.js
+++ crypto/crypto1.js
@@ -1,10 +1,38 @@
-function fromUtf8(text) {
-    const encoder = new TextEncoder()
-    return encoder.encode(text)
+function bytesFromBase64(base64) {
+    const temp = atob(base64)
+    return bytesFromChars(temp)
 }
 
-function printBytes(bytes) {
-    const root = document.createElement('div')
+function bytesFromChars(text) {
+    const length = text.length
+    const result = new Uint8Array(length)
+    for (let i = 0; i < length; i++) {
+        result[i] = text.charCodeAt(i)
+    }
+    return result
+}
+
+function bytesFromUtf8(utf8) {
+    const encoder = new TextEncoder()
+    return encoder.encode(utf8)
+}
+
+function bytesToBase64(bytes) {
+    const temp = bytesToChars(bytes)
+    return btoa(temp)
+}
+
+function bytesToChars(bytes) {
+    return String.fromCharCode(...bytes)
+}
+
+function bytesToUtf8(bytes) {
+    const decoder = new TextDecoder()
+    return decoder.decode(bytes)
+}
+
+function printBytes(bytes, parent) {
+    parent.innerHTML = ''
     for (const byte of bytes) {
         const div1 = document.createElement('div')
         const div2 = document.createElement('div')
@@ -14,9 +42,8 @@ function printBytes(bytes) {
         div3.innerText = String.fromCharCode(number)
         div1.appendChild(div2)
         div1.appendChild(div3)
-        root.appendChild(div1)
+        parent.appendChild(div1)
     }
-    return root
 }
 
 function randomBytes(length) {
