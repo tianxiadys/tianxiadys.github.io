dbd18be61d10a4fe87fce0e2d219fe4bc0592ce1
gaomingyu <874774259@qq.com>
2026-05-10 22:36:59 +0800

    fixed

--- crypto/crypto1.js
+++ crypto/crypto1.js
@@ -1,8 +1,16 @@
+/**
+ * @param {String} base64
+ * @returns {Uint8Array}
+ */
 function bytesFromBase64(base64) {
     const temp = atob(base64)
     return bytesFromChars(temp)
 }
 
+/**
+ * @param {String} text
+ * @returns {Uint8Array}
+ */
 function bytesFromChars(text) {
     const length = text.length
     const result = new Uint8Array(length)
@@ -12,45 +20,76 @@ function bytesFromChars(text) {
     return result
 }
 
+/**
+ * @param {String} utf8
+ * @returns {Uint8Array}
+ */
 function bytesFromUtf8(utf8) {
     const encoder = new TextEncoder()
     return encoder.encode(utf8)
 }
 
+/**
+ * @param {Uint8Array} bytes
+ * @returns {String}
+ */
 function bytesToBase64(bytes) {
     const temp = bytesToChars(bytes)
     return btoa(temp)
 }
 
+/**
+ * @param {Uint8Array} bytes
+ * @returns {String}
+ */
 function bytesToChars(bytes) {
     return String.fromCharCode(...bytes)
 }
 
+/**
+ * @param {Uint8Array} bytes
+ * @returns {String}
+ */
 function bytesToUtf8(bytes) {
     const decoder = new TextDecoder()
     return decoder.decode(bytes)
 }
 
-function printBytes(bytes, parent) {
-    parent.innerHTML = ''
+/**
+ * @param {Uint8Array} bytes
+ * @returns {String}
+ */
+function printBytes(bytes) {
+    const root = document.createElement('div')
     for (const byte of bytes) {
         const div1 = document.createElement('div')
         const div2 = document.createElement('div')
         const div3 = document.createElement('div')
-        const number = Number(byte)
-        div2.innerText = number.toString(16)
-        div3.innerText = String.fromCharCode(number)
+        div2.innerText = byte.toString(16)
+        if (byte >= 32 && byte <= 126) {
+            div3.innerText = String.fromCharCode(byte)
+        }
         div1.appendChild(div2)
         div1.appendChild(div3)
-        parent.appendChild(div1)
+        root.appendChild(div1)
     }
+    return root.innerHTML
 }
 
+/**
+ * @param {Number} length
+ * @returns {Uint8Array}
+ */
 function randomBytes(length) {
     const bytes = new Uint8Array(length)
     return crypto.getRandomValues(bytes)
 }
 
+/**
+ * @param {Number} length
+ * @param {Array} chars
+ * @returns {String}
+ */
 function randomString(length, chars) {
     const bytes = randomBytes(length)
     const result = []
